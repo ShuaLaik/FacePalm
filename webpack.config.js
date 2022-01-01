62150ad5429e9e834835f0ffdc83997011172152
Josh <joshualaikowski@gmail.com>
2022-01-01 15:19:32 -0500

    Root Frontend Works

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+module.exports = {
+    context: __dirname,
+    entry: "./frontend/facepalm.jsx",
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { // sometimes will say qeuery to options
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    },
+    devtool: 'source-map'
+}
